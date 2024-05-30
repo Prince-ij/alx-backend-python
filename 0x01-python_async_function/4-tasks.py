39751ea5548a4d08baf37521de03728d3f1710a2
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-05-30 11:59:05 +0100

    4 tasks fix 7

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -9,7 +9,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').wait_random
 
 
-def task_wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Create several tasks using wait_random
 
