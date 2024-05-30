c680190ab52a6085216de1479cd3695bc2266304
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-05-30 10:14:19 +0100

    4-tasks.py

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Create multiple tasks with asyncio.create_tasks
+"""
+
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_n(n: int, max_delay: int) -> List[asyncio.Task]:
+    """
+    Create several tasks using wait_random
+
+    Args:
+    n (int): max delay (int): self explanatory
+
+    Returns:
+    asyncio.Task
+    """
+
+    tasks = [asyncio.create_task(wait_random(max_delay) for _ in range(n))]
+    return tasks
