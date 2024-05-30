c010f0faaa42de87716f4c0397343f9a0ee14a2e
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-05-30 18:00:16 +0100

    fixes git push

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,7 +8,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     asynchronously loops ten times and generates
     random numbers from 0 to ten
