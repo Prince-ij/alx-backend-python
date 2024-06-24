fa6c5b4bc374dff64174b90619b5f08fbe289c8c
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-06-24 22:49:27 +0100

    101-safely_get_value.py

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -2,10 +2,11 @@
 """
 Annotating a function part2 : Duck typing
 """
-from typing import Mapping, Any
+from typing import Mapping, Any, Union, TypeVar
+T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping[Any, Any], key: Any, default: Any = None) -> Any:
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[T, Any]:
     """ documentation already in module """
     if key in dct:
         return dct[key]
