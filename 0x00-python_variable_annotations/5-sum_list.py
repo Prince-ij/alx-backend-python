2e026c798b8ebb95877bfafb514c41acedf83008
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-06-24 21:39:21 +0100

    5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -3,10 +3,10 @@
 A function to sum lists
 blah blah blah, It should be annotated
 """
-from typing import List
+from typing import List, Union
 
 
-def sum_list(input_list: List[float]) -> float:
+def sum_list(input_list: List[Union[int, float]]) -> float:
     """
     Sums up values in a list
     """
