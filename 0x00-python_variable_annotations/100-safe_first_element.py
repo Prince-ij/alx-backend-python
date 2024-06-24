4e44a7791131452a30c3a91332f632e69c1ae506
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-06-24 22:23:22 +0100

    100-safe_first_element.py

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -2,10 +2,10 @@
 """
 Duck typing: The input types aren't known
 """
-from typing import Iterable, Sequence, Union, Any
+from typing import Sequence, Union, Any
 
 
-def safe_first_element(lst: Union[Iterable[Sequence], Any]) -> Any:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """ just getting the first element
     all about duck typing though
     """
