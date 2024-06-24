32f72f3931817df51d8bdb6c40600802e7c432a0
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-06-24 21:43:27 +0100

    6-sum_mixed_list.py

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -7,7 +7,7 @@ Description: sums a list of floats and ints
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: Union[List[float], List[int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """ look at the module docs
     """
     total: float = 0
