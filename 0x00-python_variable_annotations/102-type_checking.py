1e047cb66b96ae104d1f0ead249dcf43bc6ed8aa
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-06-24 23:12:44 +0100

    101-safely_get_value.py

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 """ Using mypy to correct bad annotation
 """
-from typing import Tuple
+from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> Tuple[int, ...]:
-    zoomed_in: Tuple = [
+    zoomed_in: List[int] = [
         item for item in lst
         for i in range(factor)
     ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array: Tuple[int, ...] = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
