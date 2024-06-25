3e5764e53bb42876810c0d18cdace0dd61a39b16
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-06-25 21:11:04 +0100

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,6 +19,15 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a",), 'Can\'t find path in empty map'),
+        ({"a": 1}, ("a", "b"), 'path b not present'),
+        ])
+    def test_access_nested_map_exception(self, nested_map, path, msg):
+        with self.assertRaises(KeyError) as c:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(c.exception), msg)
+
 
 if __name__ == '__main__':
     unittest.main()
