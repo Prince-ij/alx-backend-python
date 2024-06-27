c3ca8b085ac3cc994c8b9560f2af3e3cd03b8d15
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-06-27 20:02:47 +0100

    test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,22 +2,21 @@
 """ test GithubOrgClient """
 import unittest
 from unittest.mock import patch
-from utils import memoize, get_json, access_nested_map
 from parameterized import parameterized
-from client import GitHubOrgClient
+from client import GithubOrgClient
 
 
 class TestGithubOrgClient(unittest.TestCase):
     """ testing org """
-
+    
     @parameterized.expand([
         ('google', {'login': 'google'}),
         ('abc', {'login': 'abc'})
-        ])
+    ])
     @patch('client.get_json', return_value={'login': 'test'})
     def test_org(self, org_name, expected, mock_get_json):
         """ test_org implementing """
-        client = GitHubOrgClient(org_name)
+        client = GithubOrgClient(org_name)
         self.assertEqual(client.org, expected)
         labu = f"https://api.github.com/orgs/{org_name}"
         mock_get_json.assert_called_once_with(labu)
